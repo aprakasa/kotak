92362622692e875246d48594db53d044a1d0c9d8
aryaprakasa <prakasa.arya@gmail.com>
2013-10-11 10:06:31 +0700

    git init

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package 	Kotak
+ * @author   	Arya Prakasa
+ * @license  	GPL-2.0+
+ * @link     	http://themes.prakasa.me/kotak
+ */
+
+/** Exit if accessed directly */
+if ( ! defined( 'ABSPATH' ) ) exit; ?>
